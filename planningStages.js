bdbd9661b37a68f8709bd99ad96c6ca4d08b42fe
lufherna.V2 <lufherna.v2@gmail.com>
2023-12-22 17:38:16 -0500

    Added a file with some basic functionality that will be included in the application

---
+++ planningStages.js
@@ -0,0 +1,15 @@
+// What does this application need to do?
+/*
+This application will be a place where professionals can link up, talk shop, organize outings and potentially collaborate.
+The idea is to help others network and meet like minded individuals that have a common goal and need some help. 
+This can be helpful for entrepreneurs, entry-level professionals and experienced pros that want to share their
+knowledge with others. This is NOT only for tech or any white collar job, it is also for the blue collar workers that want to connect on projects.
+
+1. Needs to have a solid landing page, that has all the information about the application and how it can help people connect
+- Nav bar with About | Login | Pricing etc. (Maybe pricing can come at a later time?)
+2. Authentication. Initially for the MVP a simple email address and password will be enough. Later though I want to add Oauth (Google, Facebook, etc.)
+3. In the account dashboard you should be able to view different career groups and be able to view who are all in there, their events and potential jobs
+based on their interests.
+4. Must have a chat option along with forums and people able to make posts, add pictures, etc.
+
+*/
